ce372af964115215bba69f9c2544ff35ac66e2bb
kingsolomon-s <kingsolomon.sobande1@gmail.com>
2025-03-07 21:15:04 +0100

    feat: add showTotalScore and displayWinner functions
    - Implemented showTotalScore to calculate and display the total score
    - Added displayWinner to determine and announce the game winner

--- script.js
+++ script.js
@@ -1,18 +1,3 @@
-// console.log("Hello World!");
-// CREATE a new function "getComputerChoice"
-// The function randomly returns "rock", "paper" or "scissors"
-// CREATE a new function "getHumanChoice"
-// Inside "getHumanChoice" prompt for human input
-// MAKE "getHumanChoice" case-insensitive
-// RETURN human input
-
-// CREATE two new variables "humanScore" and "computerScore"
-
-// CREATE a new function "playRound"
-// DEFINE "humanChoice" and "computerChoice" as parameters for playRound
-// TAKE humanChoice and computerChoice as arguments
-//
-
 function getComputerChoice() {
   let randomNumber = Math.floor(Math.random() * 3);
   if (randomNumber === 0) {
@@ -42,34 +27,34 @@ let humanScore = 0;
 let computerScore = 0;
 
 function playRound(humanChoice, computerChoice) {
-  console.log(`ROUND ${roundPlayed}: `);
+  console.log(`\nROUND ${roundPlayed}: `);
   if ((humanChoice === "ROCK") & (computerChoice === "PAPER")) {
     console.log(`Your OPP chose ${computerChoice}`);
-    console.log("YOU LOSE!");
+    console.log(`You Chose ${humanChoice}`);
+    console.log("YOU LOST THE ROUND!");
     computerScore++;
   } else if ((humanChoice === "PAPER") & (computerChoice === "SCISSOR")) {
     console.log(`Your OPP chose ${computerChoice}`);
-    console.log("YOU LOSE!");
+    console.log(`You Chose ${humanChoice}`);
+    console.log("YOU LOST THE ROUND!");
     computerScore++;
   } else if ((humanChoice === "SCISSOR") & (computerChoice === "ROCK")) {
     console.log(`Your OPP chose ${computerChoice}`);
-    console.log("YOU LOSE!");
+    console.log(`You Chose ${humanChoice}`);
+    console.log("YOU LOST THE ROUND!");
     computerScore++;
   } else if (humanChoice === computerChoice) {
     console.log(`Your OPP chose ${computerChoice}`);
-    console.log("It's a DRAW!");
+    console.log(`You Chose ${humanChoice}`);
+    console.log("It's a DRAW FOR THIS ROUND!");
   } else {
     console.log(`Your OPP chose ${computerChoice}`);
-    console.log("YOU WIN!");
+    console.log(`You Chose ${humanChoice}`);
+    console.log("YOU WON THE ROUND!");
     humanScore++;
   }
   showScore();
 }
-
-// const humanSelection = getHumanChoice();
-// const computerSelection = getComputerChoice();
-
-// playRound(humanSelection, computerSelection);
 
 let roundPlayed = 1;
 
@@ -78,6 +63,8 @@ function playGame() {
     playRound(getHumanChoice(), getComputerChoice());
     roundPlayed++;
   }
+  showTotalScore();
+  displayWinner();
 }
 
 playGame();
@@ -86,4 +73,21 @@ function showScore() {
   let score;
   score = `OPP score: ${computerScore}\nYour score: ${humanScore}`;
   console.log(score);
+}
+
+function showTotalScore() {
+  let totalScore;
+  totalScore = console.log(
+    `\nTotal Score: \n\nOPP Total Score: ${computerScore} \n\nYour Total Score: ${humanScore}`
+  );
+}
+
+function displayWinner() {
+  if (computerScore > humanScore) {
+    console.log("\nOH NO! UNFORTUNATELY, YOU HAVE TO GO HARDER");
+  } else if (computerScore === humanScore) {
+    console.log("\nA TIE, WANNA GIVE IT ANOTHER TRY?");
+  } else {
+    console.log("\nCONGRATULATIONS! YOU WON");
+  }
 }
